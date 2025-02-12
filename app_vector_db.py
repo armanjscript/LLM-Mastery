c5fbc333482ee0fd7a1460dd48ae35eb4c488239
Arman Daneshdoost <armandaneshdoost@gmail.com>
2025-02-12 14:12:10 +0330

    Vector DB Chroma RAG app added

---
+++ app_vector_db.py
@@ -0,0 +1,38 @@
+import os
+from langchain_community.document_loaders import TextLoader
+import pprint
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_community import embeddings
+
+script_directory = os.path.dirname(os.path.abspath(__file__))
+
+embedding = embeddings.OllamaEmbeddings(model="nomic-embed-text:latest")
+
+data_folder = 'data'
+file_name = 'Olympic_History_Part_1.txt'
+
+file_path = os.path.join(script_directory, data_folder, file_name)
+
+loader = TextLoader(file_path=file_path, encoding='utf-8')
+data_docs = loader.load()
+
+# print(type(data_docs))
+# pprint.pprint(data_docs)
+
+text_splitter = RecursiveCharacterTextSplitter(
+ chunk_size=100,
+ chunk_overlap=20,
+ length_function=len,
+ is_separator_regex=False
+)
+
+chunks = text_splitter.split_documents(data_docs)
+
+# print(type(chunks))
+# print(chunks)
+
+vector_store = Chroma.from_documents(
+    documents=chunks,
+    embedding=embedding
+)
